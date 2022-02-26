21fd4fc374cea84dd2e64cb1bbc8148466801080
javed1af <javed1af@gmail.com>
2022-02-27 00:21:35 +0500

    initial commit

---
+++ app/Models/Task.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    // use SoftDeletes;
+
+    public $table = 'tasks';
+
+    protected $fillable = [
+        'description',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
